9d1c059d575c35b07dcf74644ffe0e6adbeb0db4
Poleric <nixonleongkb@gmail.com>
2023-08-07 00:59:57 +0800

    Refactor

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,11 @@ const nav = document.querySelector(".tabs-container");
 
 const offset = nav.offsetTop;
 
-window.addEventListener("scroll", function () { if (window.scrollY >= offset) { nav.classList.add("sticky");
-
-} else { nav.classList.remove("sticky");
-
-} });
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= offset) {
+        nav.classList.add("sticky");
+    }
+    else {
+        nav.classList.remove("sticky");
+    }
+});
