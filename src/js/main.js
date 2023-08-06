926c9900d02f7ee10b29ed9fcfe9a3ce5c57118f
HongJunChea <cheahongjun@gmail.com>
2023-08-07 00:41:20 +0800

    change >=

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ const nav = document.querySelector(".tabs-container");
 
 const offset = nav.offsetTop;
 
-window.addEventListener("scroll", function () { if (window.scrollY = offset) { nav.classList.add("sticky");
+window.addEventListener("scroll", function () { if (window.scrollY >= offset) { nav.classList.add("sticky");
 
 } else { nav.classList.remove("sticky");
 
